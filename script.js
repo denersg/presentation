9e1c68dc597b654c2c8dda666fdb2e95380273f9
Dener Silva <dener.ufes@gmail.com>
2022-03-08 18:37:21 -0300

    Iniciado o repositorio e inserindo os temas

---
+++ script.js
@@ -0,0 +1,61 @@
+// ######################## QUESTÃO 1 ########################
+
+const buttonQuestion1 = document.querySelector(".example1 button");
+
+buttonQuestion1.addEventListener("mouseover", function() {
+    this.style.backgroundColor = "#32B72F";
+});
+
+buttonQuestion1.addEventListener("mouseout", function() {
+    this.style.backgroundColor = "#42a5f5";
+});
+
+const imgQuestion1 = document.querySelector(".example1 img");
+
+imgQuestion1.addEventListener("mouseover", function() {
+    this.src = "./img/happy-cat.png";
+});
+
+imgQuestion1.addEventListener("mouseout", function() {
+    this.src = "./img/sad-cat.jpg";
+});
+
+// mousedown
+function mouseDown(event){
+    event.style.backgroundColor = "#1ec5e5";
+    event.innerHTML = "ME SOLTA!!!";
+}
+
+// mousedown
+function mouseUp(event){
+    event.style.backgroundColor="#D94A38";
+    event.innerHTML="CLICA NEU!!!"
+}
+
+// keydown
+const inputQuestion1 = document.querySelector(".keyboard-events input");
+
+inputQuestion1.addEventListener("keydown", function(e) {
+    console.log(e.key);
+
+    document.querySelector(".keyboard-events .keys").innerHTML += `${e.key} || `;
+});
+
+// keyup
+inputQuestion1.addEventListener("keyup", function(e) {
+    if(e.key === "Enter"){
+        console.log("Soltou o Enter");
+    }
+});
+
+
+// ######################## QUESTÃO 2 ########################
+
+const buttonQuestion2 = document.querySelector(".button");
+const message = document.querySelector(".message");
+buttonQuestion2.addEventListener("click", () => {
+    message.classList.toggle("shown");
+});
+
+// ######################## QUESTÃO 3 ########################
+
